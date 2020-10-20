ddf4017f473378ccdac5be3327581f61e9fcb549
Maheen Contractor <maheencontractor@gmail.com>
2020-10-20 11:24:38 -0400

    shifted createCourse functions and discussion group functions into datamapper

--- src/main/java/com/my_pls/application/components/CreateCourse.java
+++ src/main/java/com/my_pls/application/components/CreateCourse.java
@@ -1,18 +1,12 @@
 package com.my_pls.application.components;
 
-import com.my_pls.MySqlConnection;
-
+import javax.xml.crypto.Data;
 import java.net.URLDecoder;
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
-
-
-
 public class CreateCourse {
-
-    private static Connection conn = MySqlConnection.getConnection();
 
     public static LinkedHashMap<String,Boolean> findAllDays() {
         LinkedHashMap<String, Boolean> allDays = new LinkedHashMap<String, Boolean>();
@@ -24,26 +18,10 @@ public class CreateCourse {
         return allDays;
     }
 
-    public static Map<Integer, String> findAllProfs(){
-        Map<Integer,String> profs = new HashMap<Integer,String>();
-        try {
-            PreparedStatement pst = conn.prepareStatement("select profId from courses");
-            ResultSet rs = pst.executeQuery();
-            while(rs.next()) {
-                int id = rs.getInt("profId");
-                String name = Courses.findProfName(id);
-                profs.put(id,name);
-            }
-        } catch (SQLException throwables) {
-            System.out.println("Exception at get prof name "+ throwables);
-        }
-        return profs;
-    }
-
     public static Map<String,Object> postMethodDefaults(Map<String, String> formFields, String edit) {
         Map<String,Object> map = new HashMap<>();
         boolean flag = true;
-        Map<Integer,String> profs = findAllProfs();
+        Map<Integer,String> profs = DataMapper.findAllProfs();
         Map.Entry<Integer,String> entry = profs.entrySet().iterator().next();
         int prof_id = entry.getKey();
         LinkedHashMap<String, Boolean> allDays = findAllDays();
@@ -93,7 +71,8 @@ public class CreateCourse {
                     flag = false;
                 }
             } else {
-                if(LocalDate.parse(endDate).isBefore(LocalDate.parse(startDate)) || LocalDate.parse(startDate).isBefore(LocalDate.now())) {
+                if(LocalDate.parse(endDate).isBefore(LocalDate.parse(startDate))
+                        || LocalDate.parse(startDate).isBefore(LocalDate.now())) {
                     map.put("errDate", "true");
                     map.put("start_date","");
                     map.put("end_date","");
@@ -102,37 +81,13 @@ public class CreateCourse {
             }
 
             if(flag) {
-                String sqlQuery = "insert into courses (course_name, profId, meeting_days, " +
-                        "start_time, end_time, start_date, end_date, credits, total_capacity, enrolled, " +
-                        "status, obj) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
-                if(!edit.contentEquals("-1")) {
-                    int id = Integer.parseInt(edit.replaceAll(" ",""));
-
-                    sqlQuery = "update courses set course_name=?, profId=?, meeting_days=?, " +
-                            "start_time=?, end_time=?, start_date=?, end_date=?, credits=?, " +
-                            "total_capacity=?, enrolled=?, status=?, obj=? where id=" + id;
-                }
-                PreparedStatement pst = conn.prepareStatement(sqlQuery);
-                pst.setString(1, name);
-                pst.setInt(2, prof_id);
-                pst.setString(3, daysString);
-                pst.setString(4, startTime);
-                pst.setString(5, endTime);
-                pst.setString(6, startDate);
-                pst.setString(7, endDate);
-                pst.setInt(8, credits);
-                pst.setInt(9, capacity);
-                pst.setInt(10, 0);
-                pst.setString(11, "Upcoming");
-                pst.setString(12, obj);
-                int i = pst.executeUpdate();
-                if(i != 0) {
+                boolean flag2 = DataMapper.createOrUpdateCourse(edit, name, prof_id, daysString, startTime, endTime,
+                        startDate, endDate, credits, capacity, obj);
+                if(flag2) {
                     if (edit.contentEquals("-1"))
-                        flag = addDiscussionGroupForCourse(name, prof_id, daysString,
-                                startTime, endTime, startDate, endDate, credits, capacity, obj);
+                        flag = addDiscussionGroupForCourse(name, prof_id);
                 }
             }
-
         } catch (Exception e) {
             System.out.println("Exception at create courses " + e);
             flag = false;
@@ -147,86 +102,36 @@ public class CreateCourse {
     }
 
     public static Map<String,Object> editCourse(Map<String,Object> map,String id) {
-        Map<Integer,String> profs = findAllProfs();
+        Map<Integer,String> profs = DataMapper.findAllProfs();
         Map.Entry<Integer,String> entry = profs.entrySet().iterator().next();
         int prof_id = entry.getKey();
         LinkedHashMap<String, Boolean> days = findAllDays();
-        try {
-            PreparedStatement pst = conn.prepareStatement("select * from courses where id=?");
-            pst.setInt(1, Integer.parseInt(id));
-            ResultSet rs = pst.executeQuery();
-            if(rs.next()) {
-                map.put("name",rs.getString("course_name"));
-                map.put("obj",rs.getString("obj"));
-                map.put("start_date",rs.getString("start_date"));
-                map.put("end_date",rs.getString("end_date"));
-                map.put("start_time",rs.getString("start_time"));
-                map.put("end_time",rs.getString("end_time"));
-                map.put("credits",rs.getInt("credits"));
-                map.put("cap",rs.getInt("total_capacity"));
-                prof_id = rs.getInt("profId");
-                String meeting_days = rs.getString("meeting_days");
-                days.forEach((k, v)-> {
-                    if (meeting_days.contains(k)) days.put(k,true);
-                });
-            }
-        } catch (Exception e) {
-            System.out.println("Exception at edit course " + e);
-        }
+        map.putAll(DataMapper.findCourseByCourseId(id));
+        prof_id = (int) map.get("prof_id");
+        String meeting_days = String.valueOf(map.get("meeting_days"));
+        days.forEach((k, v)-> {
+            if (meeting_days.contains(k)) days.put(k,true);
+        });
         map.put("currProf",profs.get(prof_id));
-        map.put("prof_id",prof_id);
         profs.remove(prof_id);
         map.put("days",days);
         map.put("profList",profs);
         return map;
     }
 
-    public static boolean addDiscussionGroupForCourse(String name, int prof_id, String daysString,
-                                                      String startTime, String endTime, String startDate,
-                                                      String endDate, int credits, int capacity, String obj) {
+    public static boolean addDiscussionGroupForCourse(String name, int prof_id) {
         boolean flag = false;
-        try {
-            int id = 0;
-            PreparedStatement pst = conn.prepareStatement(
-                    "select id from courses where course_name=? and profId=? and meeting_days=? and " +
-                            "start_time=? and end_time=? and start_date=? and end_date=? and credits=? and " +
-                            "total_capacity=? and obj=?");
-            pst.setString(1, name);
-            pst.setInt(2, prof_id);
-            pst.setString(3, daysString);
-            pst.setString(4, startTime);
-            pst.setString(5, endTime);
-            pst.setString(6, startDate);
-            pst.setString(7, endDate);
-            pst.setInt(8, credits);
-            pst.setInt(9, capacity);
-            pst.setString(10, obj);
-            ResultSet rs = pst.executeQuery();
-            if (rs.next()) {
-                id = rs.getInt("id");
-                PreparedStatement pst2 = conn.prepareStatement(
-                        "insert into discussion_groups (name, course_id, privacy) VALUES (?,?,?)");
-                pst2.setString(1, name);
-                pst2.setInt(2, id);
-                pst2.setInt(3, 1);
-                int i = pst2.executeUpdate();
-                if (i != 0) {
-                    PreparedStatement pst3 = conn.prepareStatement("select id from discussion_groups where course_id=?");
-                    pst3.setInt(1,id);
-                    ResultSet rs2 = pst3.executeQuery();
-                    if (rs2.next()) {
-                        int d_id = rs2.getInt("id");
-                        PreparedStatement pst4 = conn.prepareStatement("insert into dg_members (user_id, dg_id) VALUES (?,?)");
-                        pst4.setInt(1,prof_id);
-                        pst4.setInt(2,d_id);
-                        int j = pst4.executeUpdate();
-                        if (j != 0) flag = true;
-                    }
+        int id = DataMapper.findLastInsertedId();
+        if (id != -1) {
+            int i = DataMapper.addDiscussionGroup(name, id, 1);
+            if (i != 0) {
+                int d_id = DataMapper.findLastInsertedId();
+                if (d_id != -1) {
+                    flag = DataMapper.addDGmember(prof_id, d_id);
                 }
             }
-        } catch (SQLException throwables) {
-            System.out.println("Exception at addDiscussionGroupForCourse "+ throwables);
         }
         return  flag;
     }
+
 }
---
+++ src/main/java/com/my_pls/application/components/DataMapper.java
@@ -0,0 +1,199 @@
+package com.my_pls.application.components;
+
+import com.my_pls.MySqlConnection;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class DataMapper {
+    private static Connection conn = MySqlConnection.getConnection();
+
+    public static boolean createOrUpdateCourse(String edit, String name, int prof_id, String daysString,
+                                               String startTime, String endTime, String startDate,
+                                               String endDate, int credits, int capacity, String obj) {
+        boolean flag = false;
+        String sqlQuery = "insert into courses (course_name, profId, meeting_days, " +
+                "start_time, end_time, start_date, end_date, credits, total_capacity, enrolled, " +
+                "status, obj) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)";
+        if(!edit.contentEquals("-1")) {
+            int id = Integer.parseInt(edit.replaceAll(" ",""));
+
+            sqlQuery = "update courses set course_name=?, profId=?, meeting_days=?, " +
+                    "start_time=?, end_time=?, start_date=?, end_date=?, credits=?, " +
+                    "total_capacity=?, enrolled=?, status=?, obj=? where id=" + id;
+        }
+        try {
+            PreparedStatement pst = conn.prepareStatement(sqlQuery);
+            pst.setString(1, name);
+            pst.setInt(2, prof_id);
+            pst.setString(3, daysString);
+            pst.setString(4, startTime);
+            pst.setString(5, endTime);
+            pst.setString(6, startDate);
+            pst.setString(7, endDate);
+            pst.setInt(8, credits);
+            pst.setInt(9, capacity);
+            pst.setInt(10, 0);
+            pst.setString(11, "Upcoming");
+            pst.setString(12, obj);
+            int i = pst.executeUpdate();
+            if (i != 0) flag = true;
+        } catch (Exception e) {
+            System.out.println("Exception in createOrUpdateCourse " + e);
+        }
+        return flag;
+    }
+    public static Map<Integer, String> findAllProfs(){
+        Map<Integer,String> profs = new HashMap<Integer,String>();
+        try {
+            PreparedStatement pst = conn.prepareStatement("select profId from courses");
+            ResultSet rs = pst.executeQuery();
+            while(rs.next()) {
+                int id = rs.getInt("profId");
+                String name = Courses.findProfName(id);
+                profs.put(id,name);
+            }
+        } catch (SQLException throwables) {
+            System.out.println("Exception at get prof name "+ throwables);
+        }
+        return profs;
+    }
+
+    public static int findCourseIdFromCourseDetails (String name, int prof_id, String daysString,
+                                                     String startTime, String endTime, String startDate,
+                                                     String endDate, int credits, int capacity, String obj) {
+        int id = -1;
+        try {
+            PreparedStatement pst = conn.prepareStatement(
+                    "select id from courses where course_name=? and profId=? and meeting_days=? and " +
+                            "start_time=? and end_time=? and start_date=? and end_date=? and credits=? and " +
+                            "total_capacity=? and obj=?");
+            pst.setString(1, name);
+            pst.setInt(2, prof_id);
+            pst.setString(3, daysString);
+            pst.setString(4, startTime);
+            pst.setString(5, endTime);
+            pst.setString(6, startDate);
+            pst.setString(7, endDate);
+            pst.setInt(8, credits);
+            pst.setInt(9, capacity);
+            pst.setString(10, obj);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                id = rs.getInt("id");
+            }
+        } catch (Exception e ) {
+            System.out.println("Exception at findCourseIdFromCourseDetails " + e);
+        }
+        return id;
+    }
+
+    public static int addDiscussionGroup (String name, int course_id, int privacy) {
+        int i = -1;
+        try {
+            PreparedStatement pst2 = conn.prepareStatement(
+                    "insert into discussion_groups (name, course_id, privacy) VALUES (?,?,?)");
+            pst2.setString(1, name);
+            pst2.setInt(2, course_id);
+            pst2.setInt(3, privacy);
+            i = pst2.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("Exception at addDiscussionGroup " + e);
+        }
+        return i;
+    }
+
+    public static int findLastInsertedId() {
+        int i = -1;
+        try {
+            PreparedStatement pst = conn.prepareStatement(
+                    "SELECT LAST_INSERT_ID();");
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                i = rs.getInt("id");
+            }
+        } catch (Exception e) {
+            System.out.println("Exception at addDiscussionGroup " + e);
+        }
+        return i;
+    }
+
+    public static boolean addDGmember(int u_id, int dg_id) {
+        boolean flag = false;
+        try {
+            PreparedStatement pst4 = conn.prepareStatement("insert into dg_members (user_id, dg_id) VALUES (?,?)");
+            pst4.setInt(1,u_id);
+            pst4.setInt(2,dg_id);
+            int j = pst4.executeUpdate();
+            if (j != 0) flag = true;
+        } catch (Exception e) {
+            System.out.println("Exception in addDGmember " + e);
+        }
+        return flag;
+    }
+
+    public static Map<String, Object> findCourseByCourseId (String id){
+        Map<String, Object> map = new HashMap<>();
+        try {
+            PreparedStatement pst = conn.prepareStatement("select * from courses where id=?");
+            pst.setInt(1, Integer.parseInt(id));
+            ResultSet rs = pst.executeQuery();
+            if(rs.next()) {
+                map.put("name",rs.getString("course_name"));
+                map.put("obj",rs.getString("obj"));
+                map.put("start_date",rs.getString("start_date"));
+                map.put("end_date",rs.getString("end_date"));
+                map.put("start_time",rs.getString("start_time"));
+                map.put("end_time",rs.getString("end_time"));
+                map.put("credits",rs.getInt("credits"));
+                map.put("cap",rs.getInt("total_capacity"));
+                int prof_id = rs.getInt("profId");
+                map.put("prof_id", prof_id);
+                String meeting_days = rs.getString("meeting_days");
+                map.put("meeting_days", meeting_days);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception at findCourseByCourseId " + e);
+        }
+        return map;
+    }
+
+    public static Map<String, Object> getGroupDetailsByGroupId(int dg_id) {
+        Map<String,Object> details = new HashMap<>();
+        try {
+            PreparedStatement pst = conn.prepareStatement("select * from discussion_groups where id=" + dg_id);
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                details.put("name", rs.getString("name"));
+                if (rs.getInt("privacy") == 1) details.put("privacy", true);
+                Integer course = rs.getInt("course_id");
+                if (course != null) details.put("course", true);
+                details.put("id", dg_id);
+            }
+        } catch (Exception e) {
+            System.out.println("Exception in getGroupDetailsByGroupId " + e);
+        }
+        return details;
+    }
+
+    public static ArrayList<Map<String, Object>> getMyDiscussionGroups(int id) {
+        ArrayList<Map<String,Object>> allGroups = new ArrayList<>();
+        try {
+            PreparedStatement pst = conn.prepareStatement("select dg_id from dg_members where user_id=" + id);
+            ResultSet rs = pst.executeQuery();
+            while (rs.next()) {
+                Map<String, Object> group = DataMapper.getGroupDetailsByGroupId(rs.getInt("dg_id"));
+                allGroups.add(group);
+            }
+        } catch (SQLException throwables) {
+            System.out.println("Error in getMyDiscussionGroups " + throwables);
+        }
+
+        return allGroups;
+    }
+}

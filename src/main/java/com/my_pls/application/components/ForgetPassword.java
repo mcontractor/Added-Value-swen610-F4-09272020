3d451c12fca8089d471a4556ad3597679df94a0f
Maheen Contractor <maheencontractor@gmail.com>
2020-10-21 20:49:13 -0400

    moved db functions to datamapper

--- src/main/java/com/my_pls/application/components/ForgetPassword.java
+++ src/main/java/com/my_pls/application/components/ForgetPassword.java
@@ -1,17 +1,11 @@
 package com.my_pls.application.components;
 
-import com.my_pls.MySqlConnection;
 import com.my_pls.securePassword;
-import com.my_pls.sendEmail;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Random;
 
 public class ForgetPassword {
     public static Map<String,Object> getMethodDefaults(String pageType) {
@@ -36,33 +30,12 @@ public class ForgetPassword {
                 map.put("success", "false");
                 map.put("succMsg", "");
             } else {
-                try {
-                    email = URLDecoder.decode(email, "UTF-8");
-                    Connection conn = MySqlConnection.getConnection();
-                    PreparedStatement pst = conn.prepareStatement("select Email, Hash, Active from user_details where Email=?");
-                    pst.setString(1, email);
-                    ResultSet rs = pst.executeQuery();
-                    if (rs.next()) {
-                        Random theRandom = new Random();
-                        String code = String.format("%04d", theRandom.nextInt(10000));
-                        PreparedStatement pst1 = conn.prepareStatement("update user_details set Active='0',Hash=? where Email=?");
-                        pst1.setString(1, code);
-                        pst1.setString(2, email);
-                        int i = pst1.executeUpdate();
-                        String body = "Here is the confirmation code to reset your password at MyPLS. Confirmation code is " + code + "\n\nVisit: " + "http://localhost:8080/forgot-password/password to reset your password";
-                        if (i != 0) {
-                            sendEmail se = new sendEmail();
-                            se.sendEmail_content(email, "Reset Password Email at MyPLS - Confirmation Code " + code, body);
-                        }
-                    } else {
-                        //Fail message if email is not found
-                        map.put("errorEmail", "display:block;margin-left:5%; width:90%");
-                        map.put("emailVal", "");
-                        map.put("success", "false");
-                        map.put("succMsg", "");
-                    }
-                } catch (Exception e) {
-                    System.out.println("Exception at Forget Passoword " + e);
+                boolean flag = DataMapper.forgetPasswordSendEmail(email);
+                if(!flag) {
+                    map.put("errorEmail", "display:block;margin-left:5%; width:90%");
+                    map.put("emailVal", "");
+                    map.put("success", "false");
+                    map.put("succMsg", "");
                 }
                 map.put("errorEmail", "");
                 map.put("success", "true");
@@ -85,26 +58,14 @@ public class ForgetPassword {
                 e.printStackTrace();
             }
             if (formFields.get("pass").equals(formFields.get("retPass")) && formFields.get("pass").length() >= 6 && confirmCode.length() == 4) {
-                try {
-                    Connection conn = MySqlConnection.getConnection();
-                    PreparedStatement pst = conn.prepareStatement("select * from user_details where Hash=? and email=? and Active='0'");
-                    pst.setString(1, confirmCode);
-                    pst.setString(2, email);
-                    ResultSet rs = pst.executeQuery();
-                    if(rs.next()) {
-                        String newPassword = formFields.get("pass");
-                        newPassword = pwd_manager.hashPassword(newPassword);
-                        PreparedStatement pst1 = conn.prepareStatement("update user_details set Active='1',Password=? where Hash=? and email=?");
-                        pst1.setString(1, newPassword);
-                        pst1.setString(2, confirmCode);
-                        pst1.setString(3, email);
-                        int i = pst1.executeUpdate();
-                        map.put("errorPassMatch", "");
-                        map.put("success", "true");
-                        map.put("succMsg", "Your password has been changed. Please log in again.");
-                    }
-                } catch(Exception e){
-                    System.out.println("Error at Forget Password " + e);
+                String newPassword = formFields.get("pass");
+                newPassword = pwd_manager.hashPassword(newPassword);
+                boolean flag = DataMapper.changePassword(confirmCode, email, newPassword);
+                if (flag) {
+                    map.put("errorPassMatch", "");
+                    map.put("success", "true");
+                    map.put("succMsg", "Your password has been changed. Please log in again.");
+                } else {
                     map.put("errorLink","true");
                     map.put("success", "false");
                     map.put("succMsg", "");
--- src/main/java/com/my_pls/application/components/Login.java
+++ src/main/java/com/my_pls/application/components/Login.java
@@ -33,35 +33,14 @@ public class Login {
                 map.put("loginErr", "");
             } else {
                 String emVal = formFields.get("email");
-                try {
-                    emVal = URLDecoder.decode(emVal, "UTF-8");
-                    Connection conn = MySqlConnection.getConnection();
-                    PreparedStatement pst = conn.prepareStatement("select * from user_details where Email=?");
-                    pst.setString(1, emVal);
-                    ResultSet rs = pst.executeQuery();
-                    if(rs.next()) {
-                        String db_password = rs.getString("Password");
-                        String input_password = formFields.get("pass");
-                        if (pwd_manager.comparePassword(db_password,input_password)){
-                            //everything good password matches with db
-                            user.setAll(rs.getString("First_Name"),
-                                    rs.getString("Last_Name"), db_password, emVal);
-                        }
-                        else
-                        {
-                            map.put("loginErr", "display:list-item;margin-left:5%");
-                            map.put("errorEmail", "");
-                            map.put("emailVal",emVal);
-                        }
-                    }
-                    else{
-                        map.put("loginErr", "display:list-item;margin-left:5%");
-                        map.put("errorEmail", "");
-                        map.put("emailVal",emVal);
-                    }
-                } catch (Exception e) {
-                    System.out.println("Error at Login post");
+                String input_password = formFields.get("pass");
+                Pair p2 = DataMapper.login(input_password, emVal, pwd_manager);
+                if (p2.fst() == null) {
+                    map.put("loginErr", "display:list-item;margin-left:5%");
+                    map.put("errorEmail", "");
+                    map.put("emailVal",emVal);
                 }
+                user = p2.snd();
             }
         } else {
             map.put("loginErr", "");

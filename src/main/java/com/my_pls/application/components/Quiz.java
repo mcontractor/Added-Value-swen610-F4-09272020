8de759ade5f8830908659a664353fccf3ce987e2
Cyril <thari69@gmail.com>
2020-11-05 17:23:31 -0500

    Quiz updates

---
+++ src/main/java/com/my_pls/application/components/Quiz.java
@@ -0,0 +1,16 @@
+package com.my_pls.application.components;
+
+import com.my_pls.MySqlConnection;
+
+public class Quiz {
+    public int quizId;
+    public int questionId;
+    public int mark;
+    public String questionText;
+    public String responseA;
+    public String responseB;
+    public String responseC;
+    public String responseD;
+    public int answer;
+
+}
